553d5da8d4423597216ec0061d62a26c50f2d6c1
Ben Lammers <21benlammers@gmail.com>
2021-06-19 13:28:19 -0400

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { Router, Route, Link } from 'svelte-navigator';
+    import Home from './components/Home.svelte';
+    import Start from './components/Start.svelte';
+
+    // export let name: string;
+</script>
+
+<main>
+    <Router>
+        <Route path="/">
+            <Home />
+        </Route>
+        <Route path="start">
+            <Start />
+        </Route>
+    </Router>
+</main>
+
+<style type="text/scss">
+    main {
+        height: 100%;
+        text-align: center;
+        background: var(--color-subtle);
+    }
+</style>
---
+++ src/components/Home.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { Link } from 'svelte-navigator';
+    import { useNavigate, useLocation } from 'svelte-navigator';
+
+    const navigate = useNavigate();
+    const location = useLocation();
+</script>
+
+<h1>Welcome to the Taste Test App</h1>
+<p>Enjoy the tastes and we'll take care of the rest</p>
+
+<Link to="/start">Start Tasting!</Link>
+
+<style lang="scss">
+    :global(a) {
+        font-size: 1.6rem;
+    }
+
+    h1 {
+        color: var(--color-primary);
+        text-transform: uppercase;
+        font-size: 6rem;
+        font-weight: 100;
+    }
+
+    p {
+        font-size: 1.6rem;
+    }
+</style>
